296210c45176d0dae6be01394263fb65412de02d
tsouria12 <tsouristanos@outlook.com>
2024-07-01 12:53:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from threading import Thread
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
@@ -12,25 +13,24 @@ def hello():
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Hello! I am your bot.')
 
-def main():
+def run_flask():
+    port = int(os.environ.get('FLASK_PORT', '5000'))
+    app.run(host="0.0.0.0", port=port)
+
+def run_telegram_bot():
     token = os.getenv('TELEGRAM_TOKEN', '6932215235:AAH0L50r7VKtBWYw6OFl7q0DTE_g4l-zLA0')
-    port = int(os.environ.get('PORT', '8443'))
-    
     updater = Updater(token)
 
     dispatcher = updater.dispatcher
-
     dispatcher.add_handler(CommandHandler("start", start))
 
     updater.start_webhook(listen="0.0.0.0",
-                          port=port,
+                          port=8443,
                           url_path=token)
     updater.bot.setWebhook(f"https://testbot-05xl.onrender.com/{token}")
-
-    app.run(host="0.0.0.0", port=port)
 
     updater.idle()
 
 if __name__ == '__main__':
-    main()
- 
+    Thread(target=run_flask).start()
+    Thread(target=run_telegram_bot).start()
